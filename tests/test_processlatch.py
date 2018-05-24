f391178f100d25f2ea144b6e2163823e3f335e6d
devlights <gsf.zero1@devlights.onmicrosoft.com>
2018-05-24 16:21:19 +0900

    update unit tests (#1)

--- tests/test_processlatch.py
+++ tests/test_processlatch.py
@@ -17,3 +17,23 @@ class TestCountDownLatch(ut.TestCase):
     def test_doctest(self):
         """doctest を実行します。"""
         ut.TextTestRunner().run(dc.DocTestSuite(pl))
+
+    def test_ctor_nagative_count(self):
+        """ctor に負のカウントを指定した場合に例外が発生することを確認します。"""
+        # arrange
+        # act
+        # assert
+        with self.assertRaises(ValueError):
+            pl.CountDownLatch(-1)
+
+    def test_count_should_not_be_negative(self):
+        """count_downを繰り返してもcountは0より小さくはならないことを確認します。"""
+        # arrange
+        sut = pl.CountDownLatch(2)
+
+        # act
+        for i in range(10):
+            sut.count_down()
+
+        # assert
+        self.assertEqual(0, sut.count)
